839a2ea11b7b6ed93e248d4490972335c4503d16
J-TestUser <jestrada@ctsformacion.com>
2026-04-01 12:49:19 +0200

    Animations

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -18,11 +18,14 @@ public class CharacterController : MonoBehaviour
     private SpriteRenderer renderer;
     private GroundSensor sensor;
 
+    private Animator _animator;
+
     void Awake()
     {
         rBody2D = GetComponent<Rigidbody2D>();
         renderer = GetComponent<SpriteRenderer>();
         sensor = GetComponentInChildren<GroundSensor>();
+        _animator = GetComponent<Animator>();
 
         moveAction = InputSystem.actions["Move"];
         jumpAction = InputSystem.actions["Jump"]; 
@@ -43,17 +46,28 @@ public class CharacterController : MonoBehaviour
         if (moveDirection.x > 0 )
         {
             renderer.flipX = false;
+
+            _animator.SetBool("IsWalking", true);
         }
 
-        if (moveDirection.x < 0 )
+        else if (moveDirection.x < 0 )
         {
             renderer.flipX = true;
+
+            _animator.SetBool("IsWalking", true);
+        }
+
+        else
+        {
+            _animator.SetBool("IsWalking", false);
         }
 
         if (jumpAction.WasPressedThisFrame() && sensor.isGrounded)
         {
             rBody2D.AddForce(Vector2.up* jumpForce, ForceMode2D.Impulse);
         }
+        _animator.SetBool("IsJumping", !sensor.isGrounded);
+        
 
     }
     void FixedUpdate()
